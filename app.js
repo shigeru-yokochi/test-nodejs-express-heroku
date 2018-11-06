88c1600cda5570111a08214aeed0419d2c3f9deb
shigeru yokochi <shigeru@yokochi.jp>
2018-11-06 21:06:56 +0900

    test2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var session = require("express-session");
+//var session = require("express-session");
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: "YOUR SECRET SALT", resave: true, saveUninitialized: true }));
+//app.use(session({ secret: "YOUR SECRET SALT", resave: true, saveUninitialized: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
