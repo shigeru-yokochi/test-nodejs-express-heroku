40dc6080a779eee752a98c35ce05e41578a77569
shigeru yokochi <shigeru@yokochi.jp>
2018-11-09 19:07:51 +0900

    app.js

--- app.js
+++ app.js
@@ -1,64 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var express = require("express");
+var cookie = require("cookie-parser");
 var session = require("express-session");
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var shop = require('./router');
-
+var bodyParser = require("body-parser");
+ 
+// Expressインスタンスを生成
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+ 
+// テンプレートエンジンの設定
+app.set("views", "./views");
+app.set("view engine", "ejs");
+ 
+// ミドルウエアの設定
+app.use("/public", express.static("public"));
+app.use(cookie());
 app.use(session({ secret: "YOUR SECRET SALT", resave: true, saveUninitialized: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use("/shop", shop);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+ 
+// ルーティングの設定
+app.use("/shop", require("./router.js"));
+ 
+// サーバー起動
+app.listen(3000);
