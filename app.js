b45d7e73c058f569b0fa33bf89d93d06beb96459
shigeru yokochi <shigeru@yokochi.jp>
2018-11-09 21:05:17 +0900

    cookie

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var path = require('path');
 //var favicon = require('serve-favicon');
 //var logger = require('morgan');
 //var cookieParser = require('cookie-parser');
-//var session = require("express-session");
+var cookie = require('cookie-parser');
+var session = require("express-session");
 var bodyParser = require('body-parser');
 
 //var routes = require('./routes/index');
