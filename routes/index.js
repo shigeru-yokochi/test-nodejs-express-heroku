0fcf549da87a3e127399cfa863c6f9deb0958639
shigeru yokochi <shigeru@yokochi.jp>
2018-11-11 00:13:23 +0900

    redirect

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //入力画面へリダイレクト
-  response.redirect("/public/script/shop/regist/input.js");
-//  res.render('index', { title: 'Express3' });
+  res.render('index', { title: 'Express3' });
 });
 
 module.exports = router;
